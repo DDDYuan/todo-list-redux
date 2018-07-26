e460f54722a9a834213554eef1aecdd7839b6ab1
Di Yuan <dyuan@thoughtworks.com>
2018-07-26 23:34:23 +0800

    fix a bug of user

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import UserContainer from "../containers/UserContainer";
 import Todos from "./Todos";
+import User from "./User";
 
 const App = () => (
   <div className="justify-content-center text-center">
     <h1 className="mb-3">Todo List</h1>
-    <UserContainer />
+    <User />
     <Todos />
   </div>
 );
