94a8124749bae304d75583d4858925cd96e6e2f0
Di Yuan <dyuan@thoughtworks.com>
2018-07-26 16:53:32 +0800

    remove react-router-redux and add connected-react-router and configured

--- src/index.js
+++ src/index.js
@@ -2,16 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 import registerServiceWorker from "./registerServiceWorker";
+
+import { createBrowserHistory } from "history";
+import {
+  routerMiddleware,
+  connectRouter,
+  ConnectedRouter
+} from "connected-react-router";
+import { Route, Switch } from "react-router";
+
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-const store = createStore(reducers);
+const history = createBrowserHistory();
+
+const store = createStore(
+  connectRouter(history)(reducers),
+  compose(applyMiddleware(routerMiddleware(history)))
+);
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route component={App} />
+      </Switch>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
 );
