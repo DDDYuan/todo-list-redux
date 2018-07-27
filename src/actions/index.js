f3375003eb25c42a3419630bd4694136c2c8f220
Di Yuan <dyuan@thoughtworks.com>
2018-07-27 16:05:09 +0800

    publish to gh-pages

--- src/actions/index.js
+++
@@ -1,6 +0,0 @@
-export const addTodo = text => ({ type: "ADD", text });
-export const editTodo = (id, text) => ({ type: "EDIT", id, text });
-export const editableStatus = (id, status) => ({ type: "EDIT_S", id, status });
-export const removeTodo = id => ({ type: "REMOVE", id });
-export const checkTodo = id => ({ type: "CHECK", id });
-export const filterTodo = filter => ({ type: "FILTER", filter });
--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import todos from "./todos";
-import user from "./user";
-
-const reducers = combineReducers({ todos, user });
-
-export default reducers;
