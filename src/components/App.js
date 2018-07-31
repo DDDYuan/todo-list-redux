6a8fb62387a52d24a516753441975d5b64288e4b
Di Yuan <dyuan@thoughtworks.com>
2018-07-31 21:04:57 +0800

    [YuanDi] use PureComponent and componentDidMount to fetch initial state

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import Todos from "./Todos";
 import User from "./User";
 import { initTodoList } from "../actions";
 import { connect } from "react-redux";
 
-export const App = ({ initTodoList }) => {
-  fetch("/api/todos")
-    .then(response => response.json())
-    .then(json => initTodoList(json));
-  return (
-    <div className="justify-content-center text-center">
-      <h1 className="mb-3">Todo List</h1>
-      <User />
-      <Todos />
-    </div>
-  );
-};
+export class App extends PureComponent {
+  componentDidMount() {
+    fetch("/api/todos")
+      .then(response => response.json())
+      .then(json => this.props.initTodoList(json));
+  }
+
+  render() {
+    return (
+      <div className="justify-content-center text-center">
+        <h1 className="mb-3">Todo List</h1>
+        <User />
+        <Todos />
+      </div>
+    );
+  }
+}
 
 export default connect(
   null,
