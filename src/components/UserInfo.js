459cbed84b223ab3f58a2751f83143ca58276e7e
Di Yuan <dyuan@thoughtworks.com>
2018-08-02 23:33:03 +0800

    [YuanDi] show user id

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -2,15 +2,15 @@ import React from "react";
 import { connect } from "react-redux";
 import { logOut } from "../actions";
 
-export const UserInfo = ({ name, logOut }) => (
+export const UserInfo = ({ name, id, logOut }) => (
   <div className="col row">
     <h4 className="col-6">
       <span className="badge badge-info">USERNAME</span>
       {" " + name}
     </h4>
-    <h4 className="col">
-      <span className="badge badge-primary">OTHERS</span>
-      ......
+    <h4 className="col-4">
+      <span className="badge badge-primary">USERID</span>
+      {" " + id}
     </h4>
     <button className="col-2 btn btn-outline-warning" onClick={logOut}>
       LOGOUT
@@ -19,6 +19,6 @@ export const UserInfo = ({ name, logOut }) => (
 );
 
 export default connect(
-  state => ({ name: state.user.name }),
+  state => ({ name: state.user.name, id: state.user.userId }),
   { logOut }
 )(UserInfo);
